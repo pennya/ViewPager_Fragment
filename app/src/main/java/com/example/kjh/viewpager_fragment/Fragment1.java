f2d653ebdb448204808087986358db94294c485a
rlawlgns077 <rlawlgns077@gamil.com>
2017-05-15 00:53:33 +0900

    Initial commit

---
+++ app/src/main/java/com/example/kjh/viewpager_fragment/Fragment1.java
@@ -0,0 +1,48 @@
+package com.example.kjh.viewpager_fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by KJH on 2017-05-15.
+ * Fragment Life Style
+ * 1. Fragment is added
+ * 2. onAttach()                    Fragment가 Activty에 붙을때 호출
+ * 3. onCreate()                    Activty에서의 onCreate()와 비슷하나, ui 관련 작업은 할 수 없다.
+ * 4. onCreateView()                Layout을 inflater을 하여 View 작업을 하는 곳
+ * 5. onActivityCreated()           Activity에서 Fragment를 모두 생성하고난 다음에 호출됨. Activty의 onCreate()에서 setContentView()한 다음과 같다
+ * 6. onStart()                     Fragment가 화면에 표시될때 호출, 사용자의 Action과 상호 작용이 불가능함
+ * 7. onResume()                    Fragment가 화면에 완전히 그렸으며, 사용자의 Action과 상호 작용이 가능함
+ * 8. Fragment is active
+ * 9. User navigates backward or fragment is removed/replaced  or Fragment is added to the back stack, then removed/replaced
+ * 10. onPause()
+ * 11. onStop()                     Fragment가 화면에서 더이상 보여지지 않게됬을때
+ * 12. onDestroy()                  View 리소스를 해제할수있도록 호출. backstack을 사용했다면 Fragment를 다시 돌아갈때 onCreateView()가 호출됨
+ * 13. onDetached()
+ * 14. Fragment is destroyed
+ */
+
+public class Fragment1 extends Fragment {
+    public Fragment1()
+    {
+        // required
+    }
+
+    // Alt + Insert , Override method click  = ctrl + o
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        RelativeLayout layout = (RelativeLayout)inflater.inflate(R.layout.fragment_fragment1, container);
+        return layout;
+    }
+}
